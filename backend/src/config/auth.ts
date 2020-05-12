f03b2410cbd3b688cd996007e6655cf0628d874a
akioishiguro <akiosantos@gmail.com>
2020-05-12 11:18:28 -0300

    Autenticação utilizando Json Web Token

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd177b4d1d9e6b6fa86521e4b3d00b029',
+    expiresIn: '1d',
+  },
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import appointmentsRouter from './appointments.routes';
 import usersRoutes from './users.routes';
+import sessionsRoutes from './sessions.routes';
 
 const routes = Router();
 
 // marcar um horario
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRoutes);
+routes.use('/sessions', sessionsRoutes);
 
 export default routes;

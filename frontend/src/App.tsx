f57d2c111c543c069a2ed88c73403cdc3c8e969a
akioishiguro <akiosantos@gmail.com>
2020-06-24 20:45:02 -0300

    Utilizando Contexto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,14 @@ import SingIn from './pages/SignIn';
 import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <AuthContext.Provider value={{ name: 'Akio' }}>
+      <SingIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );

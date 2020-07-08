9695a3c40178b6c99151a690cfc2f5b6692527b0
akioishiguro <akiosantos@gmail.com>
2020-07-08 08:16:26 -0300

    SOLID - Dependency inversion principle

--- backend/src/modules/appointments/services/CreateAppointmentServices.ts
+++ backend/src/modules/appointments/services/CreateAppointmentServices.ts
@@ -1,23 +1,23 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
+
+import AppError from '@shared/errors/AppError';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppError from '@shared/errors/AppError';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import IAppointmentsReposiry from '@modules/appointments/repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsReposiry) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date); // Agendamento comeca em hora em hora
 
     // Verifica se já existe, algum agendamento neste horario
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -26,7 +26,7 @@ class CreateAppointmentService {
     }
 
     // Ele cria um agendamento através do Id do usuario
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });

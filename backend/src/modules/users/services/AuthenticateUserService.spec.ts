bf075f9340f700f8851fc484f001644f31872ffb
akioishiguro <akiosantos@gmail.com>
2020-07-17 07:47:58 -0300

    Todos os testes de autenticação

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import FakeHashProvider from '@modules/users/provider/HashProvider/fakes/FakeHas
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUserRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import CreateUserServices from '@modules/users/services/CreateUserServices';
+import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
   it('Should be able to authenticate', async () => {
@@ -31,5 +32,50 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('Should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to authenticate with wrong password', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserServices(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@gmail.com',
+        password: 'wrong password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });

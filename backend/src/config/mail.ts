4dbbc43c866ed918c7f72799a231603b5c97a84d
akioishiguro <akiosantos@gmail.com>
2020-09-05 10:48:34 -0300

    Configurando envio de email pela amazon

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,22 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  default: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  default: {
+    from: {
+      // Email por onde irei enviar as notificações pela aws
+      email: 'akio@ishiguro.com.br',
+      name: 'Matheus Akio',
+    },
+  },
+} as IMailConfig;
